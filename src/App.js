099a0c8652a537204e30e47f146688f5121080aa
octaviagf <oagf0616@gmail.com>
2024-02-08 18:39:19 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const composers = [
+  {
+    id: 1,
+    name: "Bach",
+    period: "Baroque",
+    photo: "./img/bach.jpg",
+  },
+  {
+    id: 2,
+    name: "Handel",
+    period: "Baroque",
+    photo: "./img/handel.jpg",
+  },
+  {
+    id: 3,
+    name: "Liszt",
+    period: "Romantic",
+    photo: "./img/liszt.jpg",
+  },
+  {
+    id: 4,
+    name: "Mozart",
+    period: "Classic",
+    photo: "./img/mozart.jpg",
+  },
+  {
+    id: 5,
+    name: "Schumann",
+    period: "Romantic",
+    photo: "./img/schumann.jpg",
+  },
+  {
+    id: 6,
+    name: "Tchaicovsky",
+    period: "Baroque",
+    photo: "./img/tchaicovsky.jpg",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Flashcards />
     </div>
   );
 }
 
-export default App;
+function Flashcards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {composers.map((composer) => (
+        <div
+          key={composer.id}
+          onClick={() => handleClick(composer.id)}
+          className={composer.id === selectedId ? "selected" : ""}
+          style={{ backgroundImage: `url(${composer.photo})` }}
+        >
+          <p>{composer.id === selectedId ? composer.period : composer.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
